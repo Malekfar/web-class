8009ae01b48702bc9d33706570026d9f0e0213ca
mojtaba malekfar <malekfar@gmail.com>
2025-10-25 09:22:51 +0330

    add posts and comments tabel and model relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -14,4 +15,14 @@ class User extends Authenticatable
 
     protected $fillable = ['name', 'email', 'password'];
     protected $hidden = ['password'];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
---
+++ database/migrations/2025_10_25_084525_create_comments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+
+            // User Field creation
+            $table->unsignedBigInteger('user_id');
+
+            // User_id Constrains
+            $table->foreign('user_id')->references('id')->on('users');
+
+            // Post Field creation
+            $table->unsignedBigInteger('post_id');
+
+            // Post_id Constrains
+            $table->foreign('post_id')->references('id')->on('posts');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
